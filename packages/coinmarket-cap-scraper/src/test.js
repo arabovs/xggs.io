d254701129a2f8bf57b6a2d21c3ea7a932d389d1
arabovs <arabov.simeon1990@gmail.com>
2021-05-06 23:03:48 +0100

    add ethereum

--- packages/coinmarket-cap-scraper/src/test.js
+++ packages/coinmarket-cap-scraper/src/test.js
@@ -5,8 +5,10 @@ const URL_XE =
   "https://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=USD";
 const URL_GOOGLE = "http://www.google.com/search?q=check+ftse100";
 
-const SELECTOR_CNM =
+const SELECTOR_BTC =
   "#__next > div > div.main-content > div.sc-57oli2-0.dEqHl.cmc-body-wrapper > div > div > div.tableWrapper___3utdq.cmc-table-homepage-wrapper___22rL4 > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > a";
+const SELECTOR_ETH =
+  "#__next > div > div.main-content > div.sc-57oli2-0.dEqHl.cmc-body-wrapper > div > div > div.tableWrapper___3utdq.cmc-table-homepage-wrapper___22rL4 > table > tbody > tr:nth-child(2) > td:nth-child(4) > div > a";
 const SELECTOR_XE =
   "#__next > div:nth-child(2) > div.fluid-container__BaseFluidContainer-qoidzu-0.gJBOzk > section > div:nth-child(2) > div > main > form > div:nth-child(2) > div.result__Repulsor-sc-1bsijpp-4.dZGBTm > div:nth-child(1) > div.unit-rates___StyledDiv-sc-1dk593y-0.dEqdnx > p";
 const SELECTOR_GOOGLE =
@@ -32,7 +34,9 @@ const getPageData = async (browser, url, selector) => {
 
 (async () => {
   const browser = await puppeteer.launch();
-  const coinPrice = await getPageData(browser, URL_COINMARKETCAP, SELECTOR_CNM);
-  console.log(coinPrice);
+  const btcPrice = await getPageData(browser, URL_COINMARKETCAP, SELECTOR_BTC);
+  const ethPrice = await getPageData(browser, URL_COINMARKETCAP, SELECTOR_ETH);
+  console.log(btcPrice);
+  console.log(ethPrice);
   https: await browser.close();
 })();
