92309149559b1a15d6957abf67b79e7f6aa3f56d
arabovs <arabov.simeon1990@gmail.com>
2021-05-11 00:16:23 +0100

    add index prices

--- packages/hasura/src/hasura/hasuraHelper.js
+++ packages/hasura/src/hasura/hasuraHelper.js
@@ -4,7 +4,9 @@ const { default: gql } = require("graphql-tag");
 const updateCoinPrice = async ({ value, crypto_code, crypto_name, source }) => {
   await client.mutate({
     mutation: gql`
-      mutation inserBTCPrice($objects: [crypto_price_updates_insert_input!]!) {
+      mutation insertCryptoPrice(
+        $objects: [crypto_price_updates_insert_input!]!
+      ) {
         insert_crypto_price_updates(objects: $objects) {
           affected_rows
         }
@@ -24,4 +26,37 @@ const updateCoinPrice = async ({ value, crypto_code, crypto_name, source }) => {
   console.log("Inserted!");
 };
 
+const updateIndexPrice = async ({
+  crypto_base_code,
+  crypto_index_price,
+  index_code,
+  index_price,
+  source,
+}) => {
+  await client.mutate({
+    mutation: gql`
+      mutation insertIndexPrice(
+        $objects: [index_price_updates_insert_input!]!
+      ) {
+        insert_index_price_updates(objects: $objects) {
+          affected_rows
+        }
+      }
+    `,
+    variables: {
+      objects: [
+        {
+          crypto_base_code,
+          crypto_index_price,
+          index_code,
+          index_price,
+          source,
+        },
+      ],
+    },
+  });
+  console.log("Inserted!");
+};
+
 module.exports = updateCoinPrice;
+module.exports = updateIndexPrice;
