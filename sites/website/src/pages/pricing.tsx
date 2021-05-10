63829840ae710b195e1e4a66f7856c231434e684
arabovs <arabov.simeon1990@gmail.com>
2021-05-10 23:08:43 +0100

    add grid to pricing.tsx

--- sites/website/src/pages/pricing.tsx
+++ sites/website/src/pages/pricing.tsx
@@ -1,14 +1,24 @@
 import React from "react"
 import { useSubscription, gql, fromPromise } from "@apollo/client"
-import { Box, Grid } from "@material-ui/core"
+import { Box, Grid, Paper } from "@material-ui/core"
 import { PriceBox } from "../components/PriceBox"
 import { IndexBox } from "../components/IndexBox"
 
 const Pricing = () => {
   return (
     <Box>
-      <PriceBox crypto_code="BTC" />
-      <PriceBox crypto_code="ETH" />
+      <Grid container spacing={1}>
+        <Grid item sm={6} xs={12} style={{ height: "100%" }}>
+          <Paper>
+            <PriceBox crypto_code="BTC" />
+          </Paper>
+        </Grid>
+        <Grid item sm={6} xs={12} style={{ height: "100%" }}>
+          <Paper>
+            <PriceBox crypto_code="ETH" />
+          </Paper>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
