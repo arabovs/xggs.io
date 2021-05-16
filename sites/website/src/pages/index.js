1909e025c7ff35cb01f290e6756aa95845350d15
arabovs <arabov.simeon1990@gmail.com>
2021-05-17 00:09:44 +0100

    accordion is good

--- sites/website/src/pages/index.js
+++ sites/website/src/pages/index.js
@@ -6,9 +6,16 @@ import {
   Container,
   ThemeProvider,
   Fade,
+  Accordion,
+  Typography,
+  AccordionSummary,
+  AccordionDetails,
 } from "@material-ui/core"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import { IndexPriceGrid } from "../components/index/IndexPriceGrid"
 import { GridPrice } from "../components/GridPrice"
+import { indexTheme } from "../theme/indexTheme"
+
 const client = require("../../../../packages/hasura/src/hasura/hasuraClient")
 const { default: gql } = require("graphql-tag")
 
@@ -19,20 +26,70 @@ const Index = () => {
   }, [])
 
   return (
-    <Container style={{ backgroundColor: "DodgerBlue" }}>
-      <Box>
-        <Fade in={shouldFade}>
-          <Box>
-            <Grid container spacing={1}>
-              <Grid item sm={6} xs={12}>
-                <GridPrice crypto_code="BTC" />
-              </Grid>
-            </Grid>
-            <IndexPriceGrid />
-          </Box>
-        </Fade>
+    <Box>
+      <Box m={1} p={1}>
+        <Paper>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>FTSE Indexes</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Container style={{ backgroundColor: "DodgerBlue" }}>
+                <ThemeProvider theme={indexTheme}>
+                  <Box>
+                    <Fade in={shouldFade}>
+                      <Box>
+                        <Grid container spacing={1}>
+                          <Grid item sm={6} xs={12}>
+                            <GridPrice crypto_code="BTC" />
+                          </Grid>
+                        </Grid>
+                        <IndexPriceGrid />
+                      </Box>
+                    </Fade>
+                  </Box>
+                </ThemeProvider>
+              </Container>
+            </AccordionDetails>
+          </Accordion>
+        </Paper>
       </Box>
-    </Container>
+      <Box m={1} p={1}>
+        <Paper>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>FTSE Indexes</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Container style={{ backgroundColor: "DodgerBlue" }}>
+                <ThemeProvider theme={indexTheme}>
+                  <Box>
+                    <Fade in={shouldFade}>
+                      <Box>
+                        <Grid container spacing={1}>
+                          <Grid item sm={6} xs={12}>
+                            <GridPrice crypto_code="BTC" />
+                          </Grid>
+                        </Grid>
+                        <IndexPriceGrid />
+                      </Box>
+                    </Fade>
+                  </Box>
+                </ThemeProvider>
+              </Container>
+            </AccordionDetails>
+          </Accordion>
+        </Paper>
+      </Box>
+    </Box>
   )
 }
 
