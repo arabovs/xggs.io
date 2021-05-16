708839af14d357ccff92b0acd810deeffa8c006e
arabovs <arabov.simeon1990@gmail.com>
2021-05-16 23:36:46 +0100

    move ftse to index

--- sites/website/src/pages/index.js
+++ sites/website/src/pages/index.js
@@ -1,6 +1,39 @@
-import * as React from "react"
-import { Box } from "@material-ui/core"
+import React from "react"
+import {
+  Box,
+  Grid,
+  Paper,
+  Container,
+  ThemeProvider,
+  Fade,
+} from "@material-ui/core"
+import { IndexPriceGrid } from "../components/index/IndexPriceGrid"
+import { GridPrice } from "../components/GridPrice"
+const client = require("../../../../packages/hasura/src/hasura/hasuraClient")
+const { default: gql } = require("graphql-tag")
 
-const IndexPage = () => <Box>Help</Box>
+const Index = () => {
+  const [shouldFade, setShouldFade] = React.useState(false)
+  React.useEffect(() => {
+    setShouldFade(true)
+  }, [])
 
-export default IndexPage
+  return (
+    <Container style={{ backgroundColor: "DodgerBlue" }}>
+      <Box>
+        <Fade in={shouldFade}>
+          <Box>
+            <Grid container spacing={1}>
+              <Grid item sm={6} xs={12}>
+                <GridPrice crypto_code="BTC" />
+              </Grid>
+            </Grid>
+            <IndexPriceGrid />
+          </Box>
+        </Fade>
+      </Box>
+    </Container>
+  )
+}
+
+export default Index
