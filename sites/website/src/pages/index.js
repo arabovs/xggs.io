0f905bcb0777c31dbc5088e42fb41dd4af60e840
arabovs <arabov.simeon1990@gmail.com>
2021-05-19 00:03:55 +0100

    add some styling to index

--- sites/website/src/pages/index.js
+++ sites/website/src/pages/index.js
@@ -1,13 +1,21 @@
 import React from "react"
-import { Box } from "@material-ui/core"
+import { Box, Paper } from "@material-ui/core"
 import { IndexPriceAccordion } from "../components/index/IndexPriceAccordion"
 
 const Index = () => {
   return (
     <Box>
-      <IndexPriceAccordion index_code="FTSE100" source="LSE" />
-      <IndexPriceAccordion index_code="FTSE250" source="LSE" />
-      <IndexPriceAccordion index_code="FTSE350" source="LSE" />
+      <Box align="center">
+        <h1>Welcome to xggs.io</h1>
+        <h6>Financial Security Indeces conversion to Crypto Currency</h6>
+      </Box>
+      <Box m={1} p={1}>
+        <Paper elevation={2}>
+          <IndexPriceAccordion index_code="FTSE100" source="LSE" />
+          <IndexPriceAccordion index_code="FTSE250" source="LSE" />
+          <IndexPriceAccordion index_code="FTSE350" source="LSE" />
+        </Paper>
+      </Box>
     </Box>
   )
 }
