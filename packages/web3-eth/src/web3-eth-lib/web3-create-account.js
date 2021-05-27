ba5f9156dad92a705233c35de7af64a1df79da88
arabovs <arabov.simeon1990@gmail.com>
2021-05-27 19:08:04 +0100

    add create account

--- packages/web3-eth/src/web3-eth-lib/web3-create-account.js
+++ packages/web3-eth/src/web3-eth-lib/web3-create-account.js
@@ -2,4 +2,17 @@ var Web3 = require("web3");
 var url = "https://mainnet.infura.io/v3/0b2de5cd7452461080b641565efed8ef";
 var web3 = new Web3(url);
 
-console.log(web3.eth.accounts.create());
+const createETHAccount = () => {
+  return web3.eth.accounts.create();
+};
+
+const {
+  address,
+  privateKey,
+  signTransaction,
+  sign,
+  encrypt,
+} = createETHAccount();
+
+console.log(address);
+console.log(privateKey);
